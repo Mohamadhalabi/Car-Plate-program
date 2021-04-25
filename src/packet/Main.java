73501ed31cef6701cd86a0f6db6a5c95aebe718e
YusufCSert <yusufcansert123@gmail.com>
2021-04-25 03:12:00 +0300

    Connecting Database to Java

--- src/packet/Main.java
+++ src/packet/Main.java
@@ -1,8 +1,17 @@
 package packet;
 
+import javax.sql.rowset.JdbcRowSet;
+import javax.sql.rowset.Joinable;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+   g s = new g();
+   s.readDataBase();
+
 
     }
 }
---
+++ src/packet/SQliteAceess.java
@@ -0,0 +1,102 @@
+package packet;
+
+import java.sql.*;
+
+public class SQliteAceess {
+
+    private Connection connect = null;
+    private Statement statement = null;
+    public PreparedStatement preparedStatement = null;
+    private ResultSet resultSet = null;
+
+    public void readDataBase() throws Exception {
+        try {
+            // This will load the MySQL driver, each DB has its own driver
+            Class.forName("org.sqlite.JDBC");
+            // Setup the connection with the DB
+            connect = DriverManager.getConnection("\"jdbc:sqlite:/C:\\\\Users\\\\Yusufcan\\\\Desktop\\\\Proje\\\\base.db");
+
+            // Statements allow to issue SQL queries to the database
+            statement = connect.createStatement();
+            // Result set get the result of the SQL query
+            resultSet = statement.executeQuery("select * from base");
+            writeResultSet(resultSet);
+
+            // PreparedStatements can use variables and are more efficient
+            preparedStatement = connect.prepareStatement("insert into  base values (default, ?, ?, ?, ? , ?, ?)");
+            // "myuser, webpage, datum, summary, COMMENTS from feedback.comments");
+            // Parameters start with 1
+            preparedStatement.setString(1, "Yusuf Can Sert");
+            preparedStatement.setString(2, "Mercedes");
+            preparedStatement.setString(3, "1234");
+            preparedStatement.executeUpdate();
+
+            writeResultSet(resultSet);
+
+
+
+            resultSet = statement
+                    .executeQuery("select * from base");
+            writeMetaData(resultSet);
+
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            close();
+        }
+
+    }
+
+    private void writeMetaData(ResultSet resultSet) throws SQLException {
+        //  Now get some metadata from the database
+        // Result set get the result of the SQL query
+
+        System.out.println("The columns in the table are: ");
+
+        System.out.println("Table: " + resultSet.getMetaData().getTableName(1));
+        for  (int i = 1; i<= resultSet.getMetaData().getColumnCount(); i++){
+            System.out.println("Column " +i  + " "+ resultSet.getMetaData().getColumnName(i));
+        }
+    }
+
+    private void writeResultSet(ResultSet resultSet) throws SQLException {
+        // ResultSet is initially before the first data set
+              while (resultSet.next()) {
+             //It is possible to get the columns via name
+             //also possible to get the columns via the column number
+             //which starts at 1
+             //e.g. resultSet.getSTring(2);
+             String user = resultSet.getString("myuser");
+             String website = resultSet.getString("webpage");
+             String summary = resultSet.getString("summary");
+             Date date = resultSet.getDate("datum");
+             String comment = resultSet.getString("comments");
+             System.out.println("User: " + user);
+             System.out.println("Website: " + website);
+             System.out.println("summary: " + summary);
+             System.out.println("Date: " + date);
+             System.out.println("Comment: " + comment);
+        }
+    }
+
+    // You need to close the resultSet
+    private void close() {
+        try {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+
+            if (statement != null) {
+                statement.close();
+            }
+
+            if (connect != null) {
+                connect.close();
+            }
+        } catch (Exception e) {
+
+        }
+    }
+
+
+}
