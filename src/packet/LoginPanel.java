818f2892e16b67f333ddc9a08a9a661465b42602
Mohamadhalabi <64552050+Mohamadhalabi@users.noreply.github.com>
2021-04-27 17:51:48 +0300

    Add files via upload

--- src/packet/LoginPanel.java
+++ src/packet/LoginPanel.java
@@ -16,44 +16,50 @@ public class LoginPanel {
         loginButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
-                boolean accountFound = false; // Mark when an account is logged in
-                String usernameInput = UsernameField.getText(); // Get username input
-                String  password = PasswordField.getText(); // Get password input
-
-
                 try {
-                    // Read from registration.txt file
-                    Scanner input = new Scanner(new File("registration.txt"));
-
-                    // Reads through the lines of the text file until an account is found
-                    while (input.hasNextLine() && !accountFound) {
-                        String s = input.nextLine();
-                        String[] sArray = s.split(",");
-
-                        // Check if the user input matches the registration file information
-                        if (usernameInput.equals(sArray[0]) && password.equals(sArray[1])) {
-                            JOptionPane.showMessageDialog(null, "Login successful", "Welcome", JOptionPane.INFORMATION_MESSAGE);
-                            accountFound = true; // Found account login
 
 
-                            // Otherwise, the users input does not match an email and password
-                        } else {
-                            PasswordField.setText("");
-                            accountFound = false;
+                    boolean accountFound = false; // Mark when an account is logged in
+                    String usernameInput = UsernameField.getText(); // Get username input
+                    char[] password = passwordField.getPassword(); // Get password input
+
+                    String userpassword = String.valueOf(password);
+
+                    try {
+                        // Read from registration.txt file
+                        Scanner input = new Scanner(new File("registration.txt"));
+
+                        // Reads through the lines of the text file until an account is found
+                        while (input.hasNextLine() && !accountFound) {
+                            String s = input.nextLine();
+                            String[] sArray = s.split(",");
+
+                            // Check if the user input matches the registration file information
+                            if (usernameInput.equals(sArray[0]) && userpassword.equals(sArray[1])) {
+                                JOptionPane.showMessageDialog(null, "Login successful", "Welcome", JOptionPane.INFORMATION_MESSAGE);
+                                accountFound = true; // Found account login
+
+
+                                // Otherwise, the users input does not match an email and password
+                            } else {
+                                PasswordField.setText("");
+                                accountFound = false;
+                            }
+
                         }
 
+                        // Show error message if an account is not found
+                        if (accountFound == false) {
+
+                        }
+
+                    } catch (FileNotFoundException ex) {
+                        JOptionPane.showMessageDialog(null, "Error");
                     }
 
-                    // Show error message if an account is not found
-                    if (    accountFound == false) {
-                        JOptionPane.showMessageDialog(null, "Invalid login \nPlease check your username and password", "Error",
-                                JOptionPane.ERROR_MESSAGE);
-                        input.close(); // Close input
-                    }
-
-                } catch ( FileNotFoundException ex) {
-                    JOptionPane.showMessageDialog(null, "Error");
+                } catch(Exception et) {
+                    JOptionPane.showMessageDialog(null, "Invalid login \nPlease check your username and password", "Error",
+                            JOptionPane.ERROR_MESSAGE);
                 }
 
             }
